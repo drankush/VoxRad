df783b786f66f567b3f23fbdf1f6582137dd8f88
drankush <drankush1989@gmail.com>
2024-07-28 08:54:44 +0530

    Update config.py
    updated to support multimodla #8

--- config/config.py
+++ config/config.py
@@ -6,6 +6,9 @@ class Config:
     SELECTED_MODEL = None
     global_md_text_content = ""
     template_dropdown = None
-    settings_window = None 
+    settings_window = None
+    multimodal_pref = False  # Add this line
+    multimodal_model = None  # Add this line
+    MM_API_KEY = None  # Add this line
 
 config = Config()
