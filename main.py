9e226fabfcd786797642b532ec56a98c3ffcd0de
Ojasvi-Nagayach <96948633+Ojasvi-Nagayach@users.noreply.github.com>
2024-08-04 22:14:30 +0530

    Update main.py
    Support for #14

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ui.main_window import initialize_ui
 from config.settings import load_settings
-from utils.encryption import load_groq_key, load_openai_key
+from utils.encryption import load_transcription_key, load_text_key
 
 if __name__ == "__main__":
     # Load settings and keys on startup
