15f7ab710f2980846d5fb5709b58d1764aafd1f6
Ankush <drankush1989@gmail.com>
2024-08-12 18:26:44 +0530

    Update file_handling.py

--- utils/file_handling.py
+++ utils/file_handling.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import shutil
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
@@ -8,16 +9,42 @@ from config.config import config
 # Global variables
 template_options = []
 
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+
 def load_templates():
     """Loads templates from the 'templates' directory and updates dropdown."""
     global template_options
     template_dir = os.path.join(config.save_directory, "templates")
     if not os.path.exists(template_dir):
         os.makedirs(template_dir)  # Create the templates directory if it doesn't exist
+    
+    # List of template files to copy. Can be expanded in future for templates to be packaged.
+    template_files_to_copy = ["HRCT_Thorax.txt"]
+    
+    # Copy specified template files from resource path to the working directory
+    for template_file in template_files_to_copy:
+        source_file = resource_path(os.path.join("templates", template_file))
+        destination_file = os.path.join(template_dir, template_file)
+        if os.path.exists(source_file) and not os.path.exists(destination_file):
+            shutil.copy2(source_file, destination_file)
+            print(f"Copied {template_file} to {destination_file}")
+    
     template_files = [f for f in os.listdir(template_dir) if f.endswith((".txt", ".md"))]
     template_files.sort()  # Sort the list of template files alphabetically
     template_options = [os.path.splitext(f)[0].replace("_", " ") for f in template_files]
     update_template_dropdown()
+
 
 def update_template_dropdown():
     """Updates the options in the template dropdown menu."""
