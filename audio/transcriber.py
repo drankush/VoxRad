a9c087d3f8d16dcc67528bb5f00f9a50311e3c6a
Ankush <drankush1989@gmail.com>
2024-09-07 22:37:04 +0530

    Update transcriber.py

--- audio/transcriber.py
+++ audio/transcriber.py
@@ -1,4 +1,5 @@
 from groq import Groq
+from openai import OpenAI
 import re
 import pyperclip
 from config.config import config
@@ -7,10 +8,11 @@ from utils.file_handling import strip_markdown
 from llm.format import format_text
 import os
 from datetime import datetime
+import google.generativeai as genai
 
 def transcribe_audio(mp3_path):
-    """Transcribes the audio using Groq's API, extracting spellings from the global template."""
-    client = Groq(api_key=config.GROQ_API_KEY)
+    """Transcribes the audio using OpenAI's API, extracting spellings from the global template."""
+    client = OpenAI(api_key=config.TRANSCRIPTION_API_KEY, base_url=config.TRANSCRIPTION_BASE_URL)
     
     # Ensure global_md_text_content is initialized
     if hasattr(config, 'global_md_text_content') and config.global_md_text_content:
@@ -27,7 +29,7 @@ def transcribe_audio(mp3_path):
             update_status("Transcribing...📝")
             transcription_result = client.audio.transcriptions.create(
                 file=(mp3_path, file.read()),
-                model="whisper-large-v3",
+                model=config.SELECTED_TRANSCRIPTION_MODEL,
                 prompt=prompt_spellings, 
                 language="en",
                 temperature=0.0
@@ -42,6 +44,41 @@ def transcribe_audio(mp3_path):
         except Exception as e:
             update_status("Some error occurred. Please check both API keys are saved and/or network connection.")
             print(f"Error details: {e}")
+
+
+def mm_gemini(mp3_path):
+    """Generates text from audio using the multimodal Gemini model."""
+    try:
+        update_status("Performing AI analysis.🤖")
+        # print(f"MM_API_KEY: {config.MM_API_KEY}")  # Print MM_API_KEY
+        # print(f"Audio Path: {mp3_path}")  # Print the audio path
+        genai.configure(api_key=config.MM_API_KEY)
+        audio_file = genai.upload_file(path=mp3_path)
+        model = genai.GenerativeModel(model_name=config.multimodal_model)
+        # print(f"Multimodal Model: {config.multimodal_model}")  # Print the selected model
+        prompt = f"""
+The provided audio is as dictated by a radiologist regarding a report of radiological study. Format is according to a standard radiological report.         
+This is the report template format as chosen by the user:
+{config.global_md_text_content}
+
+                            """
+        # print(f"Prompt: {prompt}")  # Print the prompt
+        response = model.generate_content([prompt, audio_file])
+        # print(response.text)
+        # Extract the text from the response
+        if response.text:
+            stripped_text = strip_markdown(response.text)  # Strip markdown
+            pyperclip.copy(stripped_text)  # Copy to clipboard
+            save_report(stripped_text)  # Save the report
+            update_status("Your report is ready.✨")
+            return stripped_text  # Return the processed text
+        else:
+            update_status("No text returned by the multimodal model.")
+            return None
+    except Exception as e:
+        update_status(f"Failed to to generate summary. Error: {str(e)}")
+    return None
+
 
 def save_report(report_text):
     """Saves the transcribed report to a file in the reports directory."""
