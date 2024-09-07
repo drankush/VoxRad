f228e296759bb951655f306a6ce88f1a7cecbad1
Ankush <drankush1989@gmail.com>
2024-09-07 22:36:14 +0530

    Update recorder.py
    commit for alpha release

--- audio/recorder.py
+++ audio/recorder.py
@@ -4,9 +4,10 @@ import time
 import numpy as np
 import soundfile as sf
 import os
+import sys
 import subprocess
 from config.config import config  
-from audio.transcriber import transcribe_audio
+from audio.transcriber import transcribe_audio, mm_gemini
 from ui.utils import update_status, simulate_waveform, draw_straight_line, stop_waveform_simulation, start_waveform_simulation
 
 # Global variables
@@ -18,27 +19,54 @@ recording_thread = None
 pause_event = threading.Event()
 
 def record_audio():
-    """Starts audio recording after checking for Groq API key."""
+    """Starts audio recording after checking for API keys."""
     global recording, recording_thread, audio_data
-    print(f"GROQ API Key from recorder.py: {config.GROQ_API_KEY}")
-    if config.GROQ_API_KEY is None:
-        update_status("Please Save/Unlock your Transcription and Text Model API keys in Settings.")
-        return
+    
+    # Check for API keys based on multimodal preference
+    if config.multimodal_pref:
+        if config.MM_API_KEY is None:
+            update_status("Please Save/Unlock your Multimodal Model API key in Settings.")
+            return
+    else:
+        if config.TRANSCRIPTION_API_KEY is None or config.TEXT_API_KEY is None:
+            update_status("Please Save/Unlock your Transcription and Text Model API keys in Settings.")
+            return
+
     recording = True
     paused = False
     audio_data = []  # Reset audio data
-    from ui.main_window import record_button, stop_button, pause_button, canvas, root  # Import additional global variables
+    from ui.main_window import record_button, stop_button, pause_button, canvas  # Import additional global variables
     record_button['state'] = 'disabled'
     stop_button['state'] = 'normal'
     pause_button['state'] = 'normal'  # Enable pause button
-    recording_thread = threading.Thread(target=background_recording, daemon=True)
+
+    # Check if audio device is set in settings
+    if config.audio_device is None:
+        update_status("Please select an audio device in settings.")
+        return  # Stop recording if no device is selected
+
+    # Get the device index from the saved audio device name
+    device_index = None
+    devices = sd.query_devices()
+    for i, device in enumerate(devices):
+        if device['name'] == config.audio_device:
+            device_index = i
+            break
+
+    # If the device index is still None, it means the saved device is not found
+    if device_index is None:
+        update_status("Selected audio device not found. Please check settings.")
+        return 
+
+    recording_thread = threading.Thread(target=background_recording, args=(device_index,), daemon=True)
     recording_thread.start()
     update_status("Recording 🔴")
-    start_waveform_simulation(canvas, root)  # Start simulating the waveform
+    start_waveform_simulation(canvas, config.root)  # Start simulating the waveform
+
 
 def pause_audio():
     global paused, pause_event
-    from ui.main_window import record_button, stop_button, pause_button, canvas, root  # Import additional global variables
+    from ui.main_window import record_button, stop_button, pause_button, canvas  # Import additional global variables
     if not paused:
         paused = True
         pause_event.clear()
@@ -55,16 +83,22 @@ def pause_audio():
         record_button['state'] = 'disabled'
         stop_button['state'] = 'normal'
         update_status("Recording 🔴")
-        start_waveform_simulation(canvas, root)  # Resume waveform simulation
+        start_waveform_simulation(canvas, config.root)  # Resume waveform simulation
 
-def background_recording():
+
+
+
+def background_recording(device_index=None):
     global audio_data, start_time, recording, paused, pause_event
     fs = 44100  # Sample rate
     start_time = time.time()
 
     try:
         # Prepare recording stream
-        stream = sd.InputStream(samplerate=fs, channels=1, dtype='float32')
+        if device_index is not None:
+            stream = sd.InputStream(samplerate=fs, channels=1, dtype='float32', device=device_index)
+        else:
+            stream = sd.InputStream(samplerate=fs, channels=1, dtype='float32')
         stream.start()
         print("Recording started...")
 
@@ -94,6 +128,8 @@ def background_recording():
     if not recording:
         update_status("Started Processing...⚙️")
 
+
+
 def stop_recording():
     global recording, audio_data, start_time, recording_thread
     print("stop_recording called")
@@ -104,19 +140,20 @@ def stop_recording():
     stop_waveform_simulation(canvas)  # Stop the waveform simulation
     
     # Schedule a check to wait for the recording to finish non-blockingly
-    from ui.main_window import root  # Import global variable
-    root.after(100, check_recording_finished)
+    # from ui.main_window import config.root  # Import global variable
+    config.root.after(100, check_recording_finished)
 
 def check_recording_finished():
     global recording_thread
     print("check_recording_finished called")
-    from ui.main_window import root  # Import global variable
+    # from ui.main_window import config.root  # Import global variable
     if recording_thread.is_alive():
         # Check again after some time if the recording thread is still alive
-        root.after(100, check_recording_finished)
+        config.root.after(100, check_recording_finished)
     else:
         # Now that the recording thread has finished, proceed with saving and other tasks
         complete_stop_recording()
+
 
 def complete_stop_recording():
     global audio_data, start_time, recording
@@ -135,9 +172,13 @@ def complete_stop_recording():
     if audio_data:
         wav_path = save_audio_as_wav(np.vstack(audio_data), fs)
         mp3_path = convert_wav_to_mp3(wav_path)
-        threading.Thread(target=transcribe_audio, args=(mp3_path,), daemon=True).start()
+        if config.multimodal_pref:
+            config.root.after(100, lambda: mm_gemini(mp3_path))
+        else:
+            config.root.after(100, lambda: transcribe_audio(mp3_path))
     else:
         print("No audio data to save.")
+
 
 def save_audio_as_wav(audio_data, fs):
     """Saves the recorded audio to a WAV file."""
@@ -145,15 +186,44 @@ def save_audio_as_wav(audio_data, fs):
     sf.write(file_path, audio_data, fs)
     return file_path
 
+
+def get_ffmpeg_path():
+    """Return the path to the bundled ffmpeg executable."""
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    ffmpeg_dir = os.path.join(base_dir, 'bin', 'ffmpeg')
+    
+    # Check if ffmpeg is a file or a directory
+    if os.path.isdir(ffmpeg_dir):
+        # If it's a directory, look for the ffmpeg executable inside
+        if sys.platform == "win32":
+            ffmpeg_path = os.path.join(ffmpeg_dir, 'ffmpeg.exe')
+        else:
+            ffmpeg_path = os.path.join(ffmpeg_dir, 'ffmpeg')
+    else:
+        # If it's not a directory, assume ffmpeg is the executable itself
+        ffmpeg_path = ffmpeg_dir
+
+    if not os.path.exists(ffmpeg_path):
+        print(f"FFmpeg not found at: {ffmpeg_path}")
+        return None
+    
+    print(f"FFmpeg found at: {ffmpeg_path}")
+    return ffmpeg_path
+
 def convert_wav_to_mp3(wav_path):
-    """Converts a WAV file to MP3 format using ffmpeg, targeting a maximum file size."""
+    """Converts a WAV file to MP3 format using the bundled ffmpeg, targeting a maximum file size."""
+    ffmpeg_path = get_ffmpeg_path()
+    if not ffmpeg_path:
+        print("Error: FFmpeg is not found. Please ensure FFmpeg is installed and accessible.")
+        return None
+
     mp3_path = wav_path.replace(".wav", ".mp3")
     target_size_bytes = 25 * 1024 * 1024  # 25MB in bytes
     bitrate = 128  # Initial bitrate (kbps)
 
     while True:
         command = [
-            "ffmpeg",
+            ffmpeg_path,
             "-y",
             "-i",
             str(wav_path),
