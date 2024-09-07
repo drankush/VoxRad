0c18caaefe1ca3fcd682e4acb8167cf1a336ab2f
Ankush <drankush1989@gmail.com>
2024-09-07 22:43:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from ui.main_window import initialize_ui
 from config.settings import load_settings
-from utils.encryption import load_groq_key, load_openai_key
 
 if __name__ == "__main__":
     # Load settings and keys on startup
