18a506712a0fbe29de5213bde81a92d569c16ffc
drankush <drankush1989@gmail.com>
2024-12-22 20:35:44 +0530

    Update main.py
    Fixes #20

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from ui.main_window import initialize_ui
-from config.settings import load_settings
 
 if __name__ == "__main__":
-    # Load settings and keys on startup
-    load_settings()  # This initializes save_directory
 
     # Initialize the main UI
     initialize_ui()
+
